c3a1133c4b5260d56e9383bf84e8e52edf947473
cvpe <christian.vanpee@me.com>
2019-03-18 14:52:11 +0100

    Create Export script to pdf.py

---
+++ Export script to pdf.py
@@ -0,0 +1,26 @@
+# code needs to install xhtml2pdf in site-packages
+# via stash/pip install xhtml2pdf
+import editor
+import os
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
+from pygments.styles import get_style_by_name
+from xhtml2pdf import pisa
+
+# Syntax-highlight code
+# from omz code at https://forum.omz-software.com/topic/1950/syntax-highlight-python-code-on-screen-while-running
+code = editor.get_text()
+html_formatter = HtmlFormatter(style='colorful')
+highlighted_code = highlight(code, PythonLexer(), html_formatter)
+styles = html_formatter.get_style_defs()
+html = '<html><head><style>%s</style></head><body>%s</body></html>' % (styles, highlighted_code)
+
+# html -> pdf
+path = editor.get_path()
+file_name = os.path.basename(path)
+i = file_name.rfind('.')
+if i>= 0:
+	file_name = file_name[:i] + '.pdf'
+with open(file_name, 'wb') as fil:
+	pisa.CreatePDF(html,fil)
