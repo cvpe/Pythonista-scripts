8c768ba3319c2d904079624c57e2c4840b5a3f1c
cvpe <christian.vanpee@me.com>
2019-03-19 12:12:40 +0100

    Update Export script to pdf.py

--- Export script to pdf.py
+++ Export script to pdf.py
@@ -7,10 +7,27 @@ from pygments.lexers import PythonLexer
 from pygments.formatters import HtmlFormatter
 from pygments.styles import get_style_by_name
 from xhtml2pdf import pisa
+from objc_util import *
+
+def GetConsoleText():
+	win = ObjCClass('UIApplication').sharedApplication().keyWindow()
+	main_view = win.rootViewController().view()	
+	ret = '' 
+	def analyze(v):
+		ret = None
+		for sv in v.subviews():
+			if 'textview' in str(sv._get_objc_classname()).lower():
+				if 'OMTextEditorView' not in str(sv.superview()._get_objc_classname()):	# not TextView of script
+					return str(sv.text())
+			ret = analyze(sv)
+			if ret:
+				return ret
+	ret = analyze(main_view)
+	return ret
 
 # Syntax-highlight code
 # from omz code at https://forum.omz-software.com/topic/1950/syntax-highlight-python-code-on-screen-while-running
-code = editor.get_text()
+code = editor.get_text() + '\n' + GetConsoleText()
 html_formatter = HtmlFormatter(style='colorful')
 highlighted_code = highlight(code, PythonLexer(), html_formatter)
 styles = html_formatter.get_style_defs()
