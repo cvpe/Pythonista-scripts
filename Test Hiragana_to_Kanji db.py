aeebfd2ff2f72ba5ff46e3d2c47cf1e86e499aa6
cvpe <christian.vanpee@me.com>
2019-05-05 15:49:33 +0200

    Create Test Hiragana_to_Kanji db.py

---
+++ Test Hiragana_to_Kanji db.py
@@ -0,0 +1,40 @@
+import sqlite3
+import ui
+
+conn = sqlite3.connect("HiraganaToKanji.db",check_same_thread=False)
+cursor = conn.cursor()
+rows = cursor.fetchall()
+for row in rows:
+	print(row)
+	break
+
+v = ui.View()
+v.frame = (0,0,400,400)
+v.name = 'Test Hiragana_to_Kanji.db'
+
+class MyTextFieldDelegate ():
+	def textfield_should_return(textfield):
+		cursor.execute(
+			'select hiragana, kanji from Hiragana_to_Kanji where hiragana = ?',
+			(textfield.text, ))
+		t = ''
+		for row in cursor:
+			t = t + row[1] + '\n'
+		textfield.superview['tv'].text = t
+		textfield.end_editing()
+		return True
+
+tf = ui.TextField()
+tf.frame = (10,10,380,32)
+tf.delegate = MyTextFieldDelegate
+v.add_subview(tf)
+
+tv =ui.TextView(name='tv')
+tv.frame = (10,50,380,340)
+tv.background_color = 'white'
+tv.editable = False
+v.add_subview(tv)
+
+v.present('sheet')
+v.wait_modal()
+conn.close()
